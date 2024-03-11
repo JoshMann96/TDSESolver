325af24c290a3ee13b8c754d620e22253dd5565c
carbusta <gravconstant667@hotmail.com>
2024-03-11 14:36:28 -0700

    further reorganization

--- src/standalone/CommonHeader.h
+++ src/standalone/CommonHeader.h
@@ -16,13 +16,11 @@
 #include <string>
 
 #include <mpi.h>
-#include <omp.h>
 
 #ifdef USE_RESTRICT
 #else
 #define __restrict
 #endif
-
 
 #include <boost/unordered_map.hpp>
 #include <boost/assign/list_of.hpp>
@@ -38,5 +36,3 @@ inline int MPI_Root_Proc = 0;
 
 #include <boost/math/interpolators/cardinal_cubic_b_spline.hpp>
 #include <boost/math/tools/rational.hpp>
-#include "MathTools.h"
-#include "PhysCon.h"
