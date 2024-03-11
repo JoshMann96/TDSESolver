fb20140dc181a9abc91b147924a432fdb0ed0841
carbusta <gravconstant667@hotmail.com>
2024-03-11 14:36:28 -0700

    further reorganization

---
+++ src/core/CommonHeader.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <stdio.h>
+#include <fstream>
+#include <iostream>
+#include <climits>
+#include <vector>
+#include <complex>
+#include <cmath>
+#include <algorithm>
+#include <chrono>
+#define NOMINMAX
+#include <iomanip>
+#include <future>
+#include <cstring>
+#include <string>
+
+#include <omp.h>
+
+#ifdef USE_RESTRICT
+#else
+#define __restrict
+#endif
+
+
+#include <boost/unordered_map.hpp>
+#include <boost/assign/list_of.hpp>
+
+#include <boost/math/interpolators/cardinal_cubic_b_spline.hpp>
+#include <boost/math/tools/rational.hpp>
+#include "MathTools.h"
+#include "PhysCon.h"
