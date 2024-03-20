37c12785477a28ce3687e2274ecd7e3a8303e392
carbusta <gravconstant667@hotmail.com>
2024-03-19 22:53:58 -0700

    added Basic measurer

--- src/tdsepy/PyMeasurers.cpp
+++ src/tdsepy/PyMeasurers.cpp
@@ -5,8 +5,8 @@ using namespace Measurers;
 void init_Measurers(py::module &m) {
     py::class_<Measurer>(m, "Measurer");
 
-    py::class_<PyConstant Measurer>(m, "Constant")
-        .def(py::init<double, std::string, std::string>, R"V0G0N(
+    py::class_<PyConstant, Measurer>(m, "Constant")
+        .def(py::init<double, std::string, std::string>(), R"V0G0N(
             Records a constant.
 
             Parameters
@@ -21,5 +21,28 @@ void init_Measurers(py::module &m) {
             Returns
             -------
             Constant)V0G0N",
-            "ef"_a);
+            "c"_a, "fileName"_a, "fol"_a);
+    
+    py::class_<PyBasic, Measurer>(m, "Basic")
+        .def(py::init<PySimulation*, std::string>(), R"V0G0N(
+            Records basic simulation parameters:
+             - nPts     - Number of points in grid
+             - Header   - Header name ("_py_" for Python-based runs)
+             - NSteps   - Number of time steps
+             - DX       - Spatial step size
+             - DT       - Temporal step size
+             - XS       - Array of grid points
+             - TS       - Array of time steps
+
+            Parameters
+            ----------
+            sim : Simulation
+                Associated simulation.
+            fol : str
+                Directory to contain file.
+
+            Returns
+            -------
+            Basic)V0G0N",
+            "sim"_a, "fol"_a);
 }
--- src/tdsepy/PyMeasurers.h
+++ src/tdsepy/PyMeasurers.h
@@ -1,15 +1,22 @@
 #pragma once
 #include "PyCommon.h"
 #include "Measurers.h"
+#include "PySimulation.h"
 
 void init_Measurers(py::module &m);
 
 using namespace Measurers;
 
 class PyConstant
-    : DoubleConst {
+    : public DoubleConst {
         public:
         PyConstant(double c, std::string filName, std::string fol)
             : DoubleConst(c, filName.c_str(), fol.c_str()){}
     };
 
+class PyBasic
+    : public BasicMeasurers{
+        public:
+        PyBasic(PySimulation* sim, std::string fol)
+            : BasicMeasurers("_py_", sim->getNumPoints(), sim->getDX(), sim->getDT(), sim->getXPtr(), fol.c_str()){}
+    };
