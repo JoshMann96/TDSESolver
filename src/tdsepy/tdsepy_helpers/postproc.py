da7e1bfa2490cf95a871559ad96f482627d56907
carbusta <gravconstant667@hotmail.com>
2024-03-29 15:29:13 -0700

    can now plot electron density

--- src/tdsepy/tdsepy_helpers/postproc.py
+++ src/tdsepy/tdsepy_helpers/postproc.py
@@ -1,2 +1,19 @@
 import json
+from .rawdataload import *
+import matplotlib.pyplot as plt
 
+def plotElectronDensity(fol, elecNum = -1, vmin=20, vmax=27, cmap="magma"):
+    dat, xs, ts, _ = getPsi2t(fol)
+    wghts, _ = getWghts(fol)
+    
+    print(dat.shape)
+    print(wghts.shape)
+    
+    #retain weight only for desired states
+    if elecNum != -1:
+        desWght = wghts[elecNum]
+        wghts *= 0
+        wghts[elecNum] = desWght
+    
+    s = plt.pcolormesh(xs, ts, np.log10(np.tensordot(wghts, dat, (0,0))), cmap=cmap, vmin=vmin, vmax=vmax)
+    plt.show()
