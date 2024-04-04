4feec698d4bccbbe02f1c667a42774362000117e
carbusta <gravconstant667@hotmail.com>
2024-04-04 00:10:26 -0700

    getFluxSpectrum now runs... need to test

--- src/tdsepy/tdsepy_helpers/postproc.py
+++ src/tdsepy/tdsepy_helpers/postproc.py
@@ -18,22 +18,34 @@ def plotElectronDensity(fol, elecNum = -1, vmin=20, vmax=27, cmap="magma"):
     s = plt.pcolormesh(xs, ts, np.log10(np.tensordot(wghts, dat, (0,0))), cmap=cmap, vmin=vmin, vmax=vmax)
     plt.show()
     
-def getFluxSpectrum(fol, vdNum = 0, elecNum = -1, minE = 0, maxE = 500*1.602e-19):
-    dftl, dftr, maxE_, _ = getFluxSpecVD(fol, vdNum)
-    dx = getConstant("dx", fol)
-    dt = getConstant("dt", fol)
-    wghts = getWghts(fol)
+def getFluxSpectrum(fol:str, vdNum = 0, elecNum = -1, minE = 0, maxE:float = 500):
+    """Gets the bidirectional density flux spectrum with respect to the signed kinetic energy (sgn(E) = sgn(k))
+
+    Args:
+        fol (str): Folder containing data.
+        vdNum (int, optional): Virtual detector index. Defaults to 0.
+        elecNum (int | list, optional): Selected electron states. -1 to include all, or a list to include selected states. Defaults to -1.
+        minE (int, optional) [eV]: Minimum signed kinetic energy. Defaults to 0.
+        maxE (float, optional) [eV]: Maximum signed kinetic energy. Defaults to 500 eV.
+    Returns:
+        es [eV]: Signed kinetic energy.
+        yld [ 1 / m^2 eV ]: Bidirectional flux spectrum.
+    """
+    dftl, dftr, maxE_ = getFluxSpecVD(fol, vdNum)[0:3]
+    dx,_ = getConstant("dx", fol)
+    dt,_ = getConstant("dt", fol)
+    wghts,_ = getWghts(fol)
     
-    nSamp = len(dftl)/len(wghts)
+    nSamp = dftl.shape[1]
     
     ces = maxE_/nSamp * np.arange(1, nSamp)
-    ces = ces[ces <= np.max(abs(minE), abs(maxE))]
+    ces = ces[ces <= max(abs(minE), abs(maxE))]
     ks = np.sqrt(2*9.11e-31*ces*1.602e-19/1.11212e-68)
     ces = ces[ks < np.pi/dx]
     ks = ks[ks < np.pi/dx]
     
-    dftl = dftl[:,2:len(ces)+1]*dt
-    dftr = dftr[:,2:len(ces)+1]*dt
+    dftl = dftl[:,1:nSamp]*dt
+    dftr = dftr[:,1:nSamp]*dt
     
     phi = np.exp(0.5j*dx*ks)
     phist = np.conj(phi)
@@ -42,4 +54,4 @@ def getFluxSpectrum(fol, vdNum = 0, elecNum = -1, minE = 0, maxE = 500*1.602e-19
     r = (phist*dftl - phi*dftr  )*muldiv
     l = (-phi*dftl  + phist*dftr)*muldiv
     
-    nes = np.concatenate(np.flip(ces), 0, ces)
+    nes = np.concatenate((np.flip(ces), np.array([0]), ces))
