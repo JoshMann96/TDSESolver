093dc40b42ee749f390235c6e21cb8db1ca62ac5
carbusta <gravconstant667@hotmail.com>
2024-06-03 17:01:00 -0700

    better python structure

--- src/tdsepy/PyMain.cpp
+++ src/tdsepy/PyMain.cpp
@@ -3,7 +3,7 @@
 #include "PyPotentials.h"
 #include "PyMeasurers.h"
 
-PYBIND11_MODULE(tdsepy, m) {
+PYBIND11_MODULE(_tdsepy, m) {
     py::module_ mp = m.def_submodule("Potentials", "Potentials for TDSE solver.");
     py::module_ mm = m.def_submodule("Measurers", "Measurers for TDSE solver.");
 
