e26dcd6a3312c75e611c2d40c9abb34daa58ef74
carbusta <gravconstant667@hotmail.com>
2024-12-06 16:14:13 -0800

    implemented more BCs, allowed for multiple electrons

--- src/core/FDBCs.cpp
+++ src/core/FDBCs.cpp
@@ -1,19 +1,24 @@
 #include "CORECommonHeader.h"
 #include "PhysCon.h"
+#include "MathTools.h"
 #include "FDBCs.h"
 
 namespace FD_BCs{
-    HDTransparentBC::HDTransparentBC(int order, double dx, double dt) : order(order), dx(dx / PhysCon::a0), dt(dt * PhysCon::hbar / PhysCon::auE_ha) {
+    HDTransparentBC::HDTransparentBC(int order, int nElec, double dx, double dt) : order(order), nElec(nElec), dx(dx / PhysCon::a0), dt(dt / PhysCon::hbar * PhysCon::auE_ha) {
         if (order < 1)
             throw std::invalid_argument("Order of HDTransparentBC must be greater than 0.");
 
-        psis = new CyclicArray<std::complex<double>>(order);
-        kernel = new CyclicArray<std::complex<double>>(order);
+        psis = new CyclicArray<std::complex<double>>*[nElec];
+        for (int i = 0; i < nElec; i++)
+            psis[i] = new CyclicArray<std::complex<double>>(order, 0.0);
+        kernel = new CyclicArray<std::complex<double>>(order, 0.0);
     }
 
     HDTransparentBC::~HDTransparentBC() {
-        sq_free(psis);
-        sq_free(kernel);
+        for (int i = 0; i < nElec; i++)
+            delete psis[i];
+        delete[] psis;
+        delete kernel;
     }
 
     void HDTransparentBC::calcKernel(double vb){
@@ -29,14 +34,41 @@ namespace FD_BCs{
         kernel->set(1, al * std::exp(-PhysCon::im*2.0*phi) * 0.5 * (mu*mu - 1.0));
         for (int i = 2; i < order; i++)
             kernel->set(i, (2.0*i-1.0)/(i+1.0) * mu / lam * kernel->get(i-1) - (i-2.0)/(i+1.0) / (lam*lam) * kernel->get(i-2));
+        
+        for (int i = 0; i < order; i++)
+            kernel->set(i, kernel->get(i) * std::exp(-PhysCon::im/PhysCon::hbar*(vb*i*dt)));
     }
 
+    void HDTransparentBC::getRHS(std::complex<double>* psibd, std::complex<double>* psiad, double vb, std::complex<double>* res, int nElec){
+        if (this->nElec != nElec)
+            throw std::invalid_argument("Number of electrons in HDTransparentBC does not match the number of electrons in the system.");
+
+        for (int i = 0; i < nElec; i++)
+            res[i] = CyclicArray<std::complex<double>>::inner(kernel, psis[i]) - psiad[i];
+    }
 }
 
 int main(int argc, char** argv){
-    FD_BCs::HDTransparentBC *bc = new FD_BCs::HDTransparentBC(10, 0.1*PhysCon::a0, 0.1*PhysCon::auE_ha / PhysCon::hbar);
-    bc->calcKernel(3.0);
-    bc->printKernel();
-    // (0.921767,0.313862) (0.229096,0.196664) (-0.0452012,0.0583993) (0.0434235,0.0302205) (-0.0252054,0.0404439) (0.00784178,0.00435176) (-0.0135082,0.0275446) (-0.00605452,-0.00259745) (-0.00616852,0.0166615) (-0.0114493,-0.00359059) 
+    int ne = 10;
+    FD_BCs::UniqueBC* bc = new FD_BCs::HDTransparentBC(1000, ne, 0.1*PhysCon::a0, 0.1*PhysCon::hbar/PhysCon::auE_ha);
+    std::complex<double>* res = new std::complex<double>[ne];
+    std::complex<double>* psibd = new std::complex<double>[ne];
+    std::complex<double>* psiad = new std::complex<double>[ne];
+    int time = 0;
+    for (int i = 0; i < 100; i++){
+        for (int j = 0; j < ne; j++){
+            psibd[j] = std::exp(PhysCon::im*(i/100.0));
+            psiad[j] = std::exp(PhysCon::im*(i/100.0+0.01));
+        }
+        std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+        bc->prepareStep(psibd, psiad, i/100.0);
+        bc->getRHS(psibd, psiad, i/100.0, res, ne);
+        bc->finishStep(psibd, psiad, i/100.0);
+        std::chrono::steady_clock::time_point ended = std::chrono::steady_clock::now();
+        vtlsPrnt::printArray(ne, res);
+        time += std::chrono::duration_cast<std::chrono::microseconds>(ended - begin).count();
+    }
+    std::cout << "calc time = " << time << " [us]" << std::endl;
+    
     delete bc;
 }
--- src/core/FDBCs.h
+++ src/core/FDBCs.h
@@ -11,45 +11,72 @@ namespace FD_BCs
         T* arr;
     public:
         CyclicArray(int size) : size(size), idx(0), arr((T*)sq_malloc(sizeof(T) * size)) {};
+		CyclicArray(int size, T val) : size(size), idx(0), arr((T*)sq_malloc(sizeof(T) * size)) { std::fill_n(arr, size, val); };
         ~CyclicArray() { sq_free(arr); };
         void push(T val) { arr[idx] = val; step(); };
         void set(int i, T val) { arr[(idx + i) % size] = val; };
         void step() { idx = (idx + 1) % size; };
         T get(int i) { return arr[(idx + i) % size]; };
-        void print() { for (int i = 0; i < size; i++) std::cout << get(i) << " "; std::cout << std::endl; };
+        void print() { for (int i = 0; i < size; i++) std::cout << arr[i] << " "; std::cout << std::endl; };
+
+		void mul(T val) { for (int i = 0; i < size; i++) arr[i] *= val; };
+
+		template <typename U, typename V>
+		static decltype(std::declval<U&>()* std::declval<V&>()) inner(CyclicArray<U>* arr1, CyclicArray<V>* arr2) {
+			if (arr1->size != arr2->size)
+				throw std::invalid_argument("Arrays must be of the same size.");
+
+			decltype(std::declval<U&>()* std::declval<V&>()) sum = 0;
+			if (arr1->idx == arr2->idx) // aligned arrays lead to faster code
+				for (int i = 0; i < arr1->size; i++)
+					sum += arr1->arr[i] * arr2->arr[i];
+			else
+				for (int i = 0; i < arr1->size; i++)
+					sum += arr1->get(i) * arr2->get(i);
+			return sum;
+		}
     };
 
 	enum class BC_Side {
 		LEFT, RIGHT
 	};
 
-	/*
-	Calculates LHS matrix elements and RHS value for a boundary condition.
-	*/
-	class FD_BC 
+	class BoundaryCondition
 	{
 	public:
-		virtual std::complex<double> getLHSEle(std::complex<double> psibd, std::complex<double> psiad, double vb) = 0; // LHS matrix element at the boundary position
-		virtual std::complex<double> getLHSAdjEle(std::complex<double> psibd, std::complex<double> psiad, double vb) = 0; // LHS matrix element adjacent to the boundary position
-		virtual std::complex<double> getRHS(std::complex<double> psibd, std::complex<double> psiad, double vb) = 0; // RHS value for the condition
-        virtual void iterate(std::complex<double> psibd, std::complex<double> psiad, double vb) = 0;
+		virtual std::complex<double> getLHSEle() = 0; // LHS matrix element at the boundary position
+		virtual std::complex<double> getLHSAdjEle() = 0; // LHS matrix element adjacent to the boundary position
+		virtual void getRHS(std::complex<double>* psibd, std::complex<double>* psiad, double vb, std::complex<double>* res, int nElec) = 0; // RHS value for the condition
+	};
+
+	/*
+	Boundary condition which is the same for all orbitals
+	*/
+	class CommonBC :
+		public BoundaryCondition
+	{
+	public:
+		void getRHS(std::complex<double>* psibd, std::complex<double>* psiad, double vb, std::complex<double>* res, int nElec) {
+			for (int i = 0; i < nElec; i++)
+				res[i] = getRHS(vb);
+		}
+		virtual std::complex<double> getRHS(double vb) = 0; // RHS value for the condition
 	};
 
 	class DirichletBC :
-		public FD_BC
+		public CommonBC
 	{
 	private:
 		std::complex<double> bdVal;
 	public:
 		DirichletBC(std::complex<double> bdVal) : bdVal(bdVal) {};
-		std::complex<double> getLHSEle(std::complex<double> psibd, std::complex<double> psiad, double vb) { return 1.0; };
-		std::complex<double> getLHSAdjEle(std::complex<double> psibd, std::complex<double> psiad, double vb) { return 0.0; };
-		std::complex<double> getRHS(std::complex<double> psibd, std::complex<double> psiad, double vb) { return bdVal; };
-        void iterate(std::complex<double> psibd, std::complex<double> psiad, double vb) {};
+		std::complex<double> getLHSEle() { return 1.0; };
+		std::complex<double> getLHSAdjEle() { return 0.0; };
+		std::complex<double> getRHS(double vb) { return bdVal; };
 	};
 
 	class NeumannBC :
-		public FD_BC
+		public CommonBC
 	{
 	private:
 		std::complex<double> bdDer;
@@ -57,38 +84,55 @@ namespace FD_BCs
 		double direction;
 	public:
 		NeumannBC(std::complex<double> bdDer, double dx, BC_Side side) : bdDer(bdDer), dx(dx), direction(side == BC_Side::LEFT ? 1 : -1) {};
-		std::complex<double> getLHSEle(std::complex<double> psibd, std::complex<double> psiad, double vb) { return -1.0 / dx * direction; };
-		std::complex<double> getLHSAdjEle(std::complex<double> psibd, std::complex<double> psiad, double vb) { return 1.0 / dx * direction; };
-		std::complex<double> getRHS(std::complex<double> psibd, std::complex<double> psiad, double vb) { return bdDer; };
+		std::complex<double> getLHSEle() { return -1.0 / dx * direction; };
+		std::complex<double> getLHSAdjEle() { return 1.0 / dx * direction; };
+		std::complex<double> getRHS(double vb) { return bdDer; };
         void iterate(std::complex<double> psibd, std::complex<double> psiad, double vb) {};
+	};
+
+	// Unique Boundary Condition which depends on the individual orbital
+	class UniqueBC :
+		public BoundaryCondition
+	{
+	public:
+		virtual void finishStep(std::complex<double>* psibd, std::complex<double>* psiad, double vb) = 0;
+		virtual void prepareStep(std::complex<double>* psibd, std::complex<double>* psiad, double vb) = 0;
 	};
 
 	// Homogeneous Discrete Transparent Boundary Condition
 	class HDTransparentBC :
-		public FD_BC
+		public UniqueBC
 	{
 	private:
-        int order;
+        int order, nElec;
         double dx, dt;
-        CyclicArray<std::complex<double>> *psis, *kernel;
+        CyclicArray<std::complex<double>> **psis, *kernel;
         std::complex<double> kernel0;
 
+        void calcKernel(double vb);
 	public:
 
-        void calcKernel(double vb);
-
-		HDTransparentBC(int order, double dx, double dt);
+		HDTransparentBC(int order, int nElec, double dx, double dt);
         ~HDTransparentBC();
-		std::complex<double> getLHSEle(std::complex<double> psibd, std::complex<double> psiad, double vb) { return 1.0; };
-		std::complex<double> getLHSAdjEle(std::complex<double> psibd, std::complex<double> psiad, double vb) { return 1.0; };
-		std::complex<double> getRHS(std::complex<double> psibd, std::complex<double> psiad, double vb) { return 0.0; };
-        void iterate(std::complex<double> psibd, std::complex<double> psiad, double vb){ kernel->step(); };
-        void printKernel(){kernel->print();}
+		std::complex<double> getLHSEle() { return -kernel0; };
+		std::complex<double> getLHSAdjEle() { return 1.0; };
+		void getRHS(std::complex<double>* psibd, std::complex<double>* psiad, double vb, std::complex<double>* res, int nElec);
+        void finishStep(std::complex<double>* psibd, std::complex<double>* psiad, double vb){
+			for (int i = 0; i < nElec; i++){
+				psis[i]->mul(std::exp(PhysCon::im/PhysCon::hbar*vb*dt));
+				psis[i]->push(psibd[i]);
+			}
+		};
+		void prepareStep(std::complex<double>* psibd, std::complex<double>* psiad, double vb){
+			calcKernel(vb);
+			kernel->step();
+		};
+		void printKernel() { kernel->print(); };
 	};
 
 	// Inhomogeneous Discrete Transparent Boundary Condition (incoming current)
 	class IDTransparentBC :
-		public FD_BC
+		public HDTransparentBC
 	{
 	};
 }
